45fc772f4ba9173cf02ff66be80f63b158582a4b
郭子煊 <guozx1@yonghui.cn>
2024-07-02 14:52:17 +0800

    CODING

--- src/test/java/com/example/demo/algorithm/common/RecursionTest.java
+++ src/test/java/com/example/demo/algorithm/common/RecursionTest.java
@@ -16,9 +16,7 @@ public class RecursionTest {
 
     // 斐波那契 0112358
     public int fib(int n) {
-        if (n == 0 || n == 1) {
-            return n;
-        }
+        if (n < 2) return n;
         return fib(n - 1) + fib(n - 2);
     }
 
