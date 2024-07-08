84b0beaf7aaba61d40022d375f738c1b1c1bd391
郭子煊 <guozx1@yonghui.cn>
2024-07-08 14:14:01 +0800

    415 - 字符串相加

--- src/test/java/com/example/demo/algorithm/MathTest.java
+++ src/test/java/com/example/demo/algorithm/MathTest.java
@@ -18,7 +18,8 @@ public class MathTest {
 //        System.out.println(isHappy1(19)); // 哈希
 //        System.out.println(isPowerOfFour(16));
 //        System.out.println(sumOfTheDigitsOfHarshadNumber(18));
-        System.out.println(addStrings("1785", "9256"));
+        System.out.println(addStrings("1785", "9256")); // 栈版
+        System.out.println(addStrings1("1785", "9256")); // GPT 优化版
     }
 
     public int accountBalanceAfterPurchase(int purchaseAmount) {
@@ -123,6 +124,25 @@ public class MathTest {
         if (carry) {
             sb.append("1");
         }
+        return sb.reverse().toString();
+    }
+
+    public static String addStrings1(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1, j = num2.length() - 1, carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int n1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int n2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int sum = n1 + n2 + carry;
+
+            sb.append(sum % 10);
+            carry = sum / 10;
+
+            i--;
+            j--;
+        }
+
         return sb.reverse().toString();
     }
 
