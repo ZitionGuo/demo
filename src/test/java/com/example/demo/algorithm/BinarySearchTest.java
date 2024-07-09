e43be30c522c629aacf732742db17fdff491d9e9
郭子煊 <guozx1@yonghui.cn>
2024-07-09 16:20:27 +0800

    153 - 寻找旋转排序数组中的最小值

--- src/test/java/com/example/demo/algorithm/BinarySearchTest.java
+++ src/test/java/com/example/demo/algorithm/BinarySearchTest.java
@@ -10,7 +10,8 @@ public class BinarySearchTest {
 
     @Test
     public void test() {
-        System.out.println(firstBadVersion(5));
+//        System.out.println(firstBadVersion(5));
+        System.out.println(findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
     }
 
     public int firstBadVersion(int n) {
@@ -26,5 +27,19 @@ public class BinarySearchTest {
 
     private boolean isBadVersion(int version) {
         return version >= 4;
+    }
+
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
     }
 }
