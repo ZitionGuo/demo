905310bb9e8d1de04362b03eba2ee5885c2d4617
Zition <759573397@qq.com>
2024-07-26 10:30:40 +0800

    面试题 17.15 - 最长单词

--- src/test/java/com/example/demo/algorithm/RecursionTest.java
+++ src/test/java/com/example/demo/algorithm/RecursionTest.java
@@ -1,7 +1,10 @@
-package com.example.demo.algorithm.common;
+package com.example.demo.algorithm;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Stack;
 
 /**
@@ -14,7 +17,8 @@ public class RecursionTest {
     public void test() {
 //        System.out.println(fib(4));
 //        System.out.println(fib1(4));
-        System.out.println(decodeString("3[a2[c]]"));
+//        System.out.println(decodeString("3[a2[c]]"));
+        System.out.println(longestWord(new String[]{"cat","banana","dog","nana","walk","walker","dogwalker"}));
     }
 
     // 斐波那契 0112358
@@ -167,6 +171,50 @@ public class RecursionTest {
         }
 
         return result.toString();
+    }
+
+    /**
+     * 输入： ["cat","banana","dog","nana","walk","walker","dogwalker"]
+     * 输出： "dogwalker"
+     * 解释： "dogwalker"可由"dog"和"walker"组成。
+     */
+    public String longestWord(String[] words) {
+        //把words数组存入set中当字典用
+        HashSet<String> set = new HashSet<>(Arrays.asList(words));
+        //把字符串数组按长度进行排序，如果长度相同按字典序倒序
+        Arrays.sort(words, (s1, s2) -> {
+            if (s1.length() == s2.length()) {
+                return s2.compareTo(s1);
+            }
+            return s1.length() - s2.length();
+        });
+        System.out.println(Arrays.toString(words));
+        //从尾部开始遍历
+        for (int i = words.length - 1; i >= 0; i--) {
+            //防止整个字符串与自身匹配返回true，且后续不会用到直接删除
+            set.remove(words[i]);
+            if (check(words[i], set)) {
+                return words[i];
+            }
+        }
+        return "";
+    }
+
+    public boolean check(String s, HashSet<String> set) {
+        //s的长度为0说明整个字符串被匹配完了
+        if (s.isEmpty()) {
+            return true;
+        }
+        for (int j = 1; j <= s.length(); j++) {
+            //从0~j-1位置的字符组成的字符串是否在字典中
+            if (set.contains(s.substring(0, j))) {
+                //对子串进行递归
+                if (check(s.substring(j), set)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 
