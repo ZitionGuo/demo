7e2f5065a405d37c9093f5472f0a2390bcaa8038
郭子煊 <guozx1@yonghui.cn>
2024-07-27 11:28:54 +0800

    21 - 合并两个有序链表 92 - 反转链表

--- src/test/java/com/example/demo/algorithm/test/LinkedListTest.java
+++ src/test/java/com/example/demo/algorithm/test/LinkedListTest.java
@@ -3,6 +3,8 @@ package com.example.demo.algorithm.test;
 import lombok.Data;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 /**
  * @author guozixuan
  * @date 2024/7/27 09:33
@@ -16,7 +18,10 @@ public class LinkedListTest {
         listNode.next.next = new ListNode(3);
         listNode.next.next.next = new ListNode(4);
         listNode.next.next.next.next = new ListNode(5);
-        System.out.println(reverseList(listNode));
+//        System.out.println(reverseList(listNode));
+        ListNode listNode1 = new ListNode(4);
+//        System.out.println(mergeTwoLists(listNode, listNode1));
+        System.out.println(reverseBetween(listNode, 2, 3));
     }
 
 
@@ -65,5 +70,65 @@ public class LinkedListTest {
         return newHead;
     }
 
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+            System.out.println(dummy);
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        } else if (l2 != null) {
+            cur.next = l2;
+        }
+        return dummy.next;
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        // 找到 left 前的节点
+        ListNode pre = dummy;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        System.out.println(pre);
+
+        // 开始反转的节点
+        ListNode start = pre.next;
+        // 要反转的第一个节点的下一个节点
+        ListNode then = start.next;
+        /*
+          pre: 12345
+          start: 2345 - 245
+          then: 345 - 3245
+         */
+        for (int i = 0; i < right - left; i++) {
+            start.next = then.next;
+            then.next = pre.next;
+            pre.next = then;
+            then = start.next;
+        }
+        return dummy.next;
+    }
 
 }
